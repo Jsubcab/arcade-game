c4968fe2e29fe226fcea22075e6dde464894dc60
jsubcab <jsubcab@gmail.com>
2019-07-09 14:24:50 +0300

    .

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
-//background
 
 
-var c = document.querySelector("#c");
-var ctx = c.getContext("2d");
-
+ctx.drawImage('images/char-boy.png',0,0);
 
 // Enemies our player must avoid
 var Enemy = function() {
@@ -13,6 +10,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 10;
+    this.y= 30;
 };
 
 // Update the enemy's position, required method for game
@@ -28,13 +27,13 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//Player class
 
-class player {
-    constructor(image= 'images/char-boy.png') {
-        this.image = image;
+class Player {
+    constructor() {
+        this.sprite = 'images/char-boy.png';
+        this.x = 40;
+        this.y = 200;
     }
 
     update() {
@@ -42,28 +41,19 @@ class player {
     }
 
     render() {
-
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
     handleInput() {
 
     }
-
-
 }
-
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-const enemy = new Enemy();
-
-var image = new Image();
-ctx.drawImage(image, 0, 0, c.width, c.height);
-image.src = enemy.sprite;
-
-
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
